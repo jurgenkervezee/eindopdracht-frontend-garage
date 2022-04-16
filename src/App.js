92726a6c6a7d63e7fb344459bfce89ce687410a3
jurgenkervezee <jurgenbla@gmail.com>
2022-04-16 14:10:46 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ReceptionPage from './pages/ReceptionPage';
 import SignInPage from "./pages/SignInPage";
@@ -7,20 +7,21 @@ import TopMenu from "./components/TopMenu";
 
 function App() {
     return (
-        <Router>
-            <TopMenu />
+        <>
+            <TopMenu/ >
             <Switch>
-                <Route path="/">
-                    <HomePage/>
+                <Route exact path="/">
+                    <HomePage />
                 </Route>
                 <Route exact path="/reception">
-                    <ReceptionPage/>
+                    <ReceptionPage />
                 </Route>
                 <Route exact path="/signin">
-                    <SignInPage/>
+                    <SignInPage />
                 </Route>
             </Switch>
-        </Router>
+        </>
     );
 }
+            
 export default App;
