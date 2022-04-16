99956c1b38d07beb869e64175a89bc3a21039d91
jurgenkervezee <jurgenbla@gmail.com>
2022-04-16 13:32:16 +0200

    initiele opzet van de structuur en routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ReceptionPage from './pages/ReceptionPage';
+import SignInPage from "./pages/SignInPage";
+import TopMenu from "./components/TopMenu";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <TopMenu />
+            <Switch>
+                <Route path="/">
+                    <HomePage/>
+                </Route>
+                <Route exact path="/reception">
+                    <ReceptionPage/>
+                </Route>
+                <Route exact path="/signin">
+                    <SignInPage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
-
 export default App;
---
+++ src/pages/ReceptionPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ReceptionPage() {
+
+    return (
+        <>
+            <p>receptie</p>
+
+            <div>
+                Dit is een beveiligde pagina. Nadat je ben ingelogd als een receptioniste krijg je deze pagina te zien.
+            </div>
+        </>
+    );
+}
+
+export default ReceptionPage;
