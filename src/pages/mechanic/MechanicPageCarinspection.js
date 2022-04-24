5273e0f18b550af15ddf92b90de82c5974c7140f
jurgenkervezee <jurgenbla@gmail.com>
2022-04-24 09:21:27 +0200

    started creating interface with carparts and repair activity

--- src/pages/mechanic/MechanicPageCarinspection.js
+++ src/pages/mechanic/MechanicPageCarinspection.js
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import DisplayClientTable from "../../components/reception/DisplayClientTable";
+import DisplayClientCarinspection from "../../components/carinspection/DisplayClientCarinspection";
 
 
 function MechanicPageCarinspection() {
@@ -11,9 +12,9 @@ function MechanicPageCarinspection() {
 
 
     useEffect(() => {
+        const token = localStorage.getItem('token');
 
         async function getInspectionAppointment() {
-            const token = localStorage.getItem('token');
             try {
                 const result = await axios.get(`http://localhost:8080/api/inspections/appointmentid/${carinspectionId}`, {
                     headers: {
@@ -24,16 +25,43 @@ function MechanicPageCarinspection() {
                 console.log(result);
                 setCarinspection(result.data);
 
-
-
             } catch (e) {
                 console.error(e);
             }
-
-
         }
 
+        async function getCarParts() {
+            try {
+                const carParts = await axios.get('http://localhost:8080/api/inspections/carparts/list', {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                console.log(carParts);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        async function getRepairActivity() {
+            try {
+                const repairActivity = await axios.get('http://localhost:8080/api/inspections/repairactivity/list', {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                console.log(repairActivity);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+
         getInspectionAppointment();
+        getCarParts();
+        getRepairActivity();
+
 
     }, [carinspectionId]);
 
@@ -43,8 +71,10 @@ function MechanicPageCarinspection() {
                 <>
                     <h3>Keuringsdetails</h3>
                     <div>
-                        <DisplayClientTable client={carinspection.client}/>
-                        <p>Status: {carinspection.status.name}</p>
+                        <DisplayClientCarinspection
+                            client={carinspection.client}
+                            status={carinspection.status}
+                        />
                     </div>
                 </>
             }
